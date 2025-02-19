0e288faf569f7544006b41f0db4b473c894178e0
[Denise_limburg] <denise-limburg@gmx.de>
2025-02-19 10:38:46 +0100

    striktur funktioniert

--- Flieder-Fieber/Rechteck-Berechnung/Rechteck-Berechnung/Rechteck_Berechnung.py
+++ Flieder-Fieber/Rechteck-Berechnung/Rechteck-Berechnung/Rechteck_Berechnung.py
@@ -1,1 +1,44 @@
 
+
+x = float(input("Bitte geben Sie Ihr Mass einer Flächenseite ein:"))
+
+if(x <= 0):
+    while x <=0:
+        print("Zahl zu klein, bitte erneut eingeben:")
+        x = float (input())
+
+
+
+y = float(input("Bitte geben Sie Ihr Mass einer Flächenseite ein:"))
+
+if(y <= 0):
+    while y <=0:
+        print("Zahl zu klein, bitte erneut eingeben:")
+        y = float (input())
+
+
+
+def Flaechenberechnung(x, y):
+    Ergebnis = x*y
+    print("Ihre Fläche Beträgt:", Ergebnis)
+    return Ergebnis
+
+
+Flaechenberechnung(x, y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
