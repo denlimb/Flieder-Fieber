4f9aa36f28bf315845ee7e12dec530f0de773505
[Denise_limburg] <denise-limburg@gmx.de>
2025-02-19 10:59:55 +0100

    Endschleife hinzugefügt

--- Flieder-Fieber/Rechteck-Berechnung/Rechteck-Berechnung/Rechteck_Berechnung.py
+++ Flieder-Fieber/Rechteck-Berechnung/Rechteck-Berechnung/Rechteck_Berechnung.py
@@ -1,32 +1,44 @@
+Erneut = True
+while Erneut == True:
+    
 
 
-x = float(input("Bitte geben Sie Ihr Mass einer Flächenseite ein:"))
-
-if(x <= 0):
-    while x <=0:
-        print("Zahl zu klein, bitte erneut eingeben:")
-        x = float (input())
+    x = float(input("Bitte geben Sie Ihr Mass einer Flächenseite ein:"))
 
 
-
-y = float(input("Bitte geben Sie Ihr Mass einer Flächenseite ein:"))
-
-if(y <= 0):
-    while y <=0:
-        print("Zahl zu klein, bitte erneut eingeben:")
-        y = float (input())
+    if(x <= 0):
+        while x <=0:
+            print("Zahl zu klein, bitte erneut eingeben:")
+            x = float (input())
 
 
 
-def Flaechenberechnung(x, y):
-    Ergebnis = x*y
-    print("Ihre Fläche Beträgt:", Ergebnis)
-    return Ergebnis
+    y = float(input("Bitte geben Sie Ihr Mass einer Flächenseite ein:"))
+
+    if(y <= 0):
+        while y <=0:
+            print("Zahl zu klein, bitte erneut eingeben:")
+            y = float (input())
 
 
-Flaechenberechnung(x, y)
+
+    def Flaechenberechnung(x, y):
+        Ergebnis = x*y
+        print("Ihre Fläche Beträgt:", Ergebnis)
+        return Ergebnis
 
 
+    Flaechenberechnung(x, y)
+
+
+    
+    eingabe = int (input("Möchten Sie eine erneute Berehnung vornehmen? 1. Ja 2. Nein"))
+    if (eingabe == 1):
+        Erneut = True
+    elif(eingabe ==2):
+        Erneut = False
+        
+    
 
 
 
